29cfa0bedd07d057a20c6263b84bc0797a478265
Carlos Castillo <crscastillo@gmail.com>
2026-05-15 12:47:14 -0600

    feat: add Vercel Analytics integration for improved user insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
